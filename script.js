e3e6147f4c074af15672d02fdfa2ef64644d6e0b
Luciano Henrique <luciano.castellucy@gmail.com>
2022-08-15 04:41:13 -0300

    Cria nova condição para madrugada, altera layout do botão de hora manual e adiciona o enter para ativar a troca de horário manual

--- script.js
+++ script.js
@@ -2,57 +2,68 @@ function ativarHora() {
   const texto = document.getElementById("texto-hora");
   const imagem = document.getElementsByClassName("imagem")[0];
   const date = new Date();
-  let hora = date.getHours();
+  const hora = date.getHours();
   const minutos = date.getMinutes();
+  let horaMin = `${hora}:${minutos}`;
   let saudacao = "Olá, ";
   let emoji = "🌞";
 
   const horaManual = document.getElementById("horaManual").value;
   if (horaManual.length != 0) {
-    hora = horaManual;
+    horaMin = horaManual;
   }
 
-  if ((hora >= 0 && hora < 12) || (hora >= "00:00" && hora < "12:00")) {
-    saudacao = "Bom dia,";
+  if (horaMin >= "00:00" && horaMin < "06:59") {
+    saudacao = "Madrugouu";
+    emoji = "🌚";
+    imagem.src = "img/dawn.jpg";
+    document.body.style.backgroundImage = "url('img/sky_dawn.jpg')";
+  } else if (horaMin >= "07:00" && horaMin < "12:00") {
+    saudacao = "Bom dia";
     emoji = "🌞";
     imagem.src = "img/morning.png";
     document.body.style.backgroundImage = "url('img/sky_blue.webp')";
-  } else if (
-    (hora >= 12 && hora <= 18) ||
-    (hora >= "12:00" && hora <= "18:59")
-  ) {
-    saudacao = "Boa tarde,";
-    emoji = "🌇";
+  } else if (horaMin >= "12:00" && horaMin <= "18:59") {
+    saudacao = "Boa tarde";
+    emoji = "😎";
     imagem.src = "img/afternoon.png";
     document.body.style.backgroundImage = "url('img/sky_orange.webp')";
   } else {
-    emoji = "🌜";
-    saudacao = "Boa noite,";
+    emoji = "🌔";
+    saudacao = "Boa noite";
     imagem.src = "img/night.png";
     document.body.style.backgroundImage = "url('img/sky_night.png')";
   }
 
   //Validação para TEXTO de resultado
   if (horaManual.length == 0) {
-    texto.innerHTML = `${saudacao} agora são ${hora}h${minutos} minutos. ${emoji}`;
+    texto.innerHTML = `${saudacao}${emoji}<br>Agora são ${horaMin}hrs.`;
   } else {
-    texto.innerHTML = `${saudacao} você informou ${horaManual}hrs. ${emoji}`;
+    texto.innerHTML = `${saudacao}${emoji}<br>você informou ${horaManual}hrs.`;
   }
 }
 
 ativarHora();
 
-const btnAnimation = document.getElementsByClassName("btn-hora")[0];
-const container = document.getElementsByClassName("container")[0];
+function AnimaBtn() {
+  const btnAnimation = document.getElementsByClassName("btn-hora")[0];
+  const container = document.getElementsByClassName("container")[0];
+  const horaManual = document.getElementById("horaManual");
 
-console.log(container);
+  function refresh() {
+    container.classList.add("ativo");
 
-function refresh() {
-  container.classList.add("ativo");
+    setTimeout(function () {
+      container.classList.remove("ativo");
+    }, 500);
+  }
 
-  setTimeout(function () {
-    container.classList.remove("ativo");
-  }, 500);
+  btnAnimation.addEventListener("click", refresh);
+  horaManual.addEventListener("keypress", function (e) {
+    if (e.key == "Enter") {
+      ativarHora();
+      refresh();
+    }
+  });
 }
-
-btnAnimation.addEventListener("click", refresh);
+AnimaBtn();
